2eb89ef4b8574fe5d7e0cd2747dffb9164bad201
MatthewPryor1 <matthew.pryor@graffinity.io>
2026-05-13 15:28:17 +0100

    Initial commit

---
+++ cards.js
@@ -0,0 +1,82 @@
+const cards = [
+  {
+    "id": "card_48",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0035452_4309.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/visi116754.jpg",
+    "front_image_url_3": "",
+    "rhs_url": "https://www.rhs.org.uk/plants/155/acanthus-mollis/details",
+    "back_text": "Acanthus mollis\n\n(bear's breech)\n\nacanthus (Gr) = a prickle\n(*leaf seen on corinthian capitals)\nmollis = soft / tender / velvety"
+  },
+  {
+    "id": "card_52",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_WSYD0005811_89.JPG",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0035523_4312.jpg",
+    "front_image_url_3": "",
+    "rhs_url": "https://www.rhs.org.uk/plants/774/alchemilla-mollis/details",
+    "back_text": "Alchemilla mollis\n\n(lady’s mantle)\n\n(Arabic) = plants used in alchemy\nmollis - soft / velvet hairs on leaves"
+  },
+  {
+    "id": "card_54",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/visi128473.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/visi128475.jpg",
+    "front_image_url_3": "",
+    "rhs_url": "https://www.rhs.org.uk/plants/326211/astrantia-star-of-passion-pbr/details",
+    "back_text": "Astrantia major 'Star of Passion'\n\n(greater masterwort)\n\naster (Latin & Gr) = star-like shaped flower umbels / bracts\nmajor (La) = greater\n‘Star of Passion’ - deep burgundy - red to pink bracts = love & passion"
+  },
+  {
+    "id": "card_64",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSYD0011252_4332.jpg",
+    "front_image_url_2": "",
+    "front_image_url_3": "",
+    "rhs_url": "https://www.rhs.org.uk/plants/23474/corydalis-flexuosa/details",
+    "back_text": "Corydalis flexuosa\n\n(blue corydalis)\n\ncorydalis (Gr) = crested lark\n(flower resembles shape of its head)\nflexuosa (La) = bending\n(zig zag growth habit)"
+  },
+  {
+    "id": "card_71",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/_KOS9973.jpg",
+    "front_image_url_2": "",
+    "front_image_url_3": "",
+    "rhs_url": "https://www.rhs.org.uk/plants/343473/echinacea-purpurea-sensation-pink-pbr/details",
+    "back_text": "Echinacea purpurea 'Sensation Pink'\n\n(purple coneflower)\n\nEchinos (Gr) = hedgehog\n(spiky cone shape disk of flower, resembles spines of)\npurpurea (La) = purple"
+  },
+  {
+    "id": "card_72",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/elbo13044.jpg",
+    "front_image_url_2": "",
+    "front_image_url_3": "",
+    "rhs_url": "https://www.rhs.org.uk/plants/6785/eryngium-giganteum/details",
+    "back_text": "Eryngium giganteum\n\n(Miss Willmott’s ghost)\n\nErygion (Gr) = thistle with spiny toothed leaves\n\ngiganteum (La) = gigantic"
+  },
+  {
+    "id": "card_78",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0034357_3647.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-_TSC0986_3237.jpg",
+    "front_image_url_3": "https://apps.rhs.org.uk/plantselectorimages/detail/elbo10176.jpg",
+    "rhs_url": "https://www.rhs.org.uk/plants/9334/iris-pseudacorus/details",
+    "back_text": "Iris pseudacorus\n\n(yellow iris)\n\nIris (Gr) = rainbow\n(after the rainbow goddess)\npseudo (Gr) = false\nacorus = genus of aromatic plants"
+  },
+  {
+    "id": "card_80",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0035785_4155.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/vaem01851.jpg",
+    "front_image_url_3": "",
+    "rhs_url": "https://www.rhs.org.uk/plants/32804/leucanthemum-vulgare/details",
+    "back_text": "Leucanthemum vulgare\n\n(ox-eye daisy)\n\nLeukos (Gr) = white\nanthemon (Gr) = flower\nvulgare (La) = common"
+  },
+  {
+    "id": "card_87",
+    "front_image_url_1": "https://apps.rhs.org.uk/plantselectorimages/detail/phbe08319.jpg",
+    "front_image_url_2": "https://apps.rhs.org.uk/plantselectorimages/detail/visi104668.jpg",
+    "front_image_url_3": "",
+    "rhs_url": "https://www.rhs.org.uk/plants/54584/miscanthus-sinensis-morning-light-%28v%29/details",
+    "back_text": "Miscanthus sinensis 'Morning Light'\n\n(eulalia)\n\nMiscos (Gr) = stem\nanthos (Gr) = flower\nsinensis (La) = Chinese\n‘Morning Light’ variegated margins glow in the morning sunlight"
+  },
+  {
+    "id": "card_92",
+    "front_image_url_1": "https://www.phoenixperennials.com/wp-content/uploads/11606.jpg",
+    "front_image_url_2": "",
+    "front_image_url_3": "",
+    "rhs_url": "https://www.rhs.org.uk/plants/510747/phlox-paniculata-olympus-%28v%29/details",
+    "back_text": "Phlox paniculata 'Olympus'\n\n(perennial phlox)\n\nPhlego (Gr) = to burn\nor\nPhlox (Gr) = flame\n\npaniculata = panicle flowers"
+  }
+];
